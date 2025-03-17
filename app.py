d0bb8d7a83d530d7cda6166784958ca8d2357895
Kiran Kowda <cipherkiran@gmail.com>
2025-03-17 17:54:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import google.generativeai as genai
 from tenacity import retry, stop_after_attempt, wait_exponential
 import time
 
